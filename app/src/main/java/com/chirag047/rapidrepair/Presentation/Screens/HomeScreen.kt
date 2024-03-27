2d647de1e8f1b79548095926cb2ac8814e840e24
chiragpatel047 <chiragpatel4890@gmail.com>
2024-03-27 13:32:58 +0530

    vehicle-screen-back-1

--- app/src/main/java/com/chirag047/rapidrepair/Presentation/Screens/HomeScreen.kt
+++ app/src/main/java/com/chirag047/rapidrepair/Presentation/Screens/HomeScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -75,6 +76,7 @@ import com.chirag047.rapidrepair.Presentation.Components.poppinsText
 import com.chirag047.rapidrepair.Presentation.Components.textWithSeeAllText
 import com.chirag047.rapidrepair.Presentation.ViewModels.HomeScreenViewModel
 import com.chirag047.rapidrepair.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -90,186 +92,196 @@ fun HomeScreen(navController: NavController) {
     }
 
     LaunchedEffect(key1 = Unit) {
-        homeScreenViewModel.getCenter("Surat").collect {
-            when (it) {
-
-                is ResponseType.Success -> {
-                    val list = mutableListOf(CenterModel())
-                    list.clear()
-                    list.addAll(it.data!!)
-                    centerList.value = list
-                }
-
-                is ResponseType.Loading -> {
-
-                }
-
-                is ResponseType.Error -> {
-                    Log.d("FireBaseDataCenterData",it.errorMsg.toString())
-                }
-            }
+        CoroutineScope(Dispatchers.IO).launch {
+            homeScreenViewModel.getCenter("Surat")
         }
     }
 
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .verticalScroll(scroll)
-    ) {
+    val state = homeScreenViewModel.centers.collectAsState()
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
+    Box(Modifier.fillMaxSize()) {
 
-            Column {
-                Text(
-                    text = "Location",
-                    textAlign = TextAlign.Center,
-                    fontFamily = FontFamily(Font(R.font.poppins_medium)),
-                    fontSize = 12.sp
-                )
+        when (state.value) {
 
-                Row(verticalAlignment = Alignment.CenterVertically) {
-
-                    Icon(
-                        painter = painterResource(id = R.drawable.location_pin_icon),
-                        contentDescription = "",
-                        modifier = Modifier.size(15.dp),
-                        tint = MaterialTheme.colorScheme.onBackground
-                    )
-                    Spacer(modifier = Modifier.padding(2.dp))
-                    Text(
-                        text = "Surat City, India",
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
-                        textAlign = TextAlign.Center,
-                        fontSize = 14.sp
-                    )
-                    Spacer(modifier = Modifier.padding(2.dp))
-                    Icon(
-                        painter = painterResource(id = R.drawable.down_arrow_icon),
-                        contentDescription = "",
-                        modifier = Modifier.size(12.dp),
-                        tint = MaterialTheme.colorScheme.onBackground
-                    )
-                }
+            is ResponseType.Success -> {
+                val list = mutableListOf(CenterModel())
+                list.clear()
+                list.addAll(state.value.data!!)
+                centerList.value = list
             }
-            GrayFilledSimpleButton(imageIcon = R.drawable.notification) {
-                navController.navigate("NotificationScreen")
+
+            is ResponseType.Loading -> {
+
+            }
+
+            is ResponseType.Error -> {
+                Log.d("FireBaseDataCenterData",state.value.errorMsg.toString())
             }
         }
 
-        SearchBar("Search near services...")
 
-        Row(
+        Column(
             Modifier
                 .fillMaxWidth()
-                .padding(15.dp, 20.dp, 15.dp, 0.dp)
-                .clip(RoundedCornerShape(25.dp))
-                .background(MaterialTheme.colorScheme.tertiaryContainer),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .verticalScroll(scroll)
         ) {
-            Column(verticalArrangement = Arrangement.Center) {
-                Text(
-                    text = "Get services from",
-                    fontSize = 18.sp,
-                    fontFamily = FontFamily(Font(R.font.poppins_medium)),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(25.dp, 0.dp, 0.dp, 0.dp)
-                )
-                Text(
-                    text = "your location",
-                    fontSize = 18.sp,
-                    fontFamily = FontFamily(Font(R.font.poppins_medium)),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(25.dp, 0.dp, 0.dp, 0.dp)
-                )
 
-                Spacer(modifier = Modifier.padding(6.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
 
-                Button(
-                    onClick = {
-                    },
-                    colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary),
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .padding(25.dp, 0.dp)
-                ) {
+                Column {
                     Text(
-                        text = "Find service",
-                        fontSize = 10.sp,
-                        color = MaterialTheme.colorScheme.onTertiary,
-                        fontFamily = FontFamily(Font(R.font.poppins_medium))
+                        text = "Location",
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        fontSize = 12.sp
                     )
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+                        Icon(
+                            painter = painterResource(id = R.drawable.location_pin_icon),
+                            contentDescription = "",
+                            modifier = Modifier.size(15.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                        Spacer(modifier = Modifier.padding(2.dp))
+                        Text(
+                            text = "Surat City, India",
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                            textAlign = TextAlign.Center,
+                            fontSize = 14.sp
+                        )
+                        Spacer(modifier = Modifier.padding(2.dp))
+                        Icon(
+                            painter = painterResource(id = R.drawable.down_arrow_icon),
+                            contentDescription = "",
+                            modifier = Modifier.size(12.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                }
+                GrayFilledSimpleButton(imageIcon = R.drawable.notification) {
+                    navController.navigate("NotificationScreen")
                 }
             }
 
-            Image(
-                painter = painterResource(id = R.drawable.home_cover_image),
-                contentDescription = "",
-                modifier = Modifier.size(150.dp)
+            SearchBar("Search near services...")
+
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp, 20.dp, 15.dp, 0.dp)
+                    .clip(RoundedCornerShape(25.dp))
+                    .background(MaterialTheme.colorScheme.tertiaryContainer),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(verticalArrangement = Arrangement.Center) {
+                    Text(
+                        text = "Get services from",
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(25.dp, 0.dp, 0.dp, 0.dp)
+                    )
+                    Text(
+                        text = "your location",
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(25.dp, 0.dp, 0.dp, 0.dp)
+                    )
+
+                    Spacer(modifier = Modifier.padding(6.dp))
+
+                    Button(
+                        onClick = {
+                        },
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .padding(25.dp, 0.dp)
+                    ) {
+                        Text(
+                            text = "Find service",
+                            fontSize = 10.sp,
+                            color = MaterialTheme.colorScheme.onTertiary,
+                            fontFamily = FontFamily(Font(R.font.poppins_medium))
+                        )
+                    }
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.home_cover_image),
+                    contentDescription = "",
+                    modifier = Modifier.size(150.dp)
+                )
+
+            }
+
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            poppinsBoldText(
+                contentText = "Book a service",
+                size = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
             )
 
+            Row(
+                Modifier
+                    .fillMaxWidth()
+            ) {
+
+                SingleCardService(icon = R.drawable.services_icon,
+                    title = "Vehicle Service",
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(15.dp, 0.dp, 7.dp, 0.dp)
+                        .shadow(
+                            5.dp,
+                            RoundedCornerShape(25.dp)
+                        )
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable {
+
+                        })
+
+                SingleCardService(icon = R.drawable.towtruck_icon,
+                    title = "RSA Service",
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(7.dp, 0.dp, 15.dp, 0.dp)
+                        .shadow(
+                            5.dp,
+                            RoundedCornerShape(25.dp)
+                        )
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .clickable {
+
+                        })
+
+            }
+
+            Spacer(modifier = Modifier.padding(4.dp))
+            textWithSeeAllText(title = "Near you")
+
+            loadCenters(list = centerList.value, navController = navController)
         }
 
-        Spacer(modifier = Modifier.padding(4.dp))
-
-        poppinsBoldText(
-            contentText = "Book a service",
-            size = 18.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(15.dp)
-        )
-
-        Row(
-            Modifier
-                .fillMaxWidth()
-        ) {
-
-            SingleCardService(icon = R.drawable.services_icon,
-                title = "Vehicle Service",
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(15.dp, 0.dp, 7.dp, 0.dp)
-                    .shadow(
-                        5.dp,
-                        RoundedCornerShape(25.dp)
-                    )
-                    .clip(RoundedCornerShape(25.dp))
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .clickable {
-
-                    })
-
-            SingleCardService(icon = R.drawable.towtruck_icon,
-                title = "RSA Service",
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(7.dp, 0.dp, 15.dp, 0.dp)
-                    .shadow(
-                        5.dp,
-                        RoundedCornerShape(25.dp)
-                    )
-                    .clip(RoundedCornerShape(25.dp))
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .clickable {
-
-                    })
-
-        }
-
-        Spacer(modifier = Modifier.padding(4.dp))
-        textWithSeeAllText(title = "Near you")
-
-        loadCenters(list = centerList.value, navController = navController)
     }
+
 
 }
 
--- app/src/main/java/com/chirag047/rapidrepair/Presentation/ViewModels/HomeScreenViewModel.kt
+++ app/src/main/java/com/chirag047/rapidrepair/Presentation/ViewModels/HomeScreenViewModel.kt
@@ -1,16 +1,30 @@
 package com.chirag047.rapidrepair.Presentation.ViewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.chirag047.rapidrepair.Common.ResponseType
 import com.chirag047.rapidrepair.Model.CenterModel
 import com.chirag047.rapidrepair.Repository.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(val dataRepository: DataRepository) : ViewModel() {
 
-    suspend fun getCenter(cityName : String) = dataRepository.getCenters(cityName)
+    private val _centers = MutableStateFlow<ResponseType<List<CenterModel>>>(ResponseType.Loading())
+
+    val centers: StateFlow<ResponseType<List<CenterModel>>>
+        get() = _centers
+
+    suspend fun getCenter(cityName: String){
+        viewModelScope.launch {
+            dataRepository.getCenters(cityName).collect{
+                _centers.emit(it)
+            }
+        }
+    }
 
 }
